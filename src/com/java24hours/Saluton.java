eb583b096863322bf73aea48fc5955a91e383129
Abraham Bueno <abueno@localhost.localdomain>
2019-01-28 23:53:04 -0600

    Fixing IDE trouble

--- src/com/java24hours/Saluton.java
+++ src/com/java24hours/Saluton.java
@@ -2,6 +2,8 @@ package com.java24hours;
 
 public class Saluton {
     public static void main(String[] args) {
-        
+        String greeting = "Saluton mondo!";
+        System.out.println(greeting);
     }
+
 }
