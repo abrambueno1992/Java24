629552aa7c62e4f2518473952129da29d1449b66
Abraham Bueno <abueno@localhost.localdomain>
2019-01-30 13:56:46 -0600

    Created PlanetWeight class, using different data types and operators.

---
+++ src/com/java24hours/PlanetWeight.java
@@ -0,0 +1,18 @@
+package com.java24hours;
+
+public class PlanetWeight {
+    public static void main(String[] args) {
+        System.out.println("Your weight on Earth is ");
+        double weight = 178;
+        System.out.println(weight);
+        System.out.println("Your weight on Mercury is ");
+        double mercury = weight * .378;
+        System.out.println(mercury);
+        System.out.println("Your weight on the Moon is ");
+        double moon = weight * .166;
+        System.out.println(moon);
+        System.out.println("Your weight on Jupiter is ");
+        double jupiter = weight * 2.364;
+        System.out.println(jupiter);
+    }
+}
