36759fa8d06f9c2a020358730500981664512042
Abraham Bueno <abueno@localhost.localdomain>
2019-01-30 13:30:19 -0600

    Working on Variable class

---
+++ src/com/java24hours/Variable.java
@@ -0,0 +1,11 @@
+package com.java24hours;
+
+public class Variable {
+    public static void main(String[] args) {
+        int tops;
+        float gradePointAverage;
+        char key = 'C';
+        String productName = "Larvets";
+
+    }
+}
